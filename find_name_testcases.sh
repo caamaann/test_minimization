c86504b09413aa9da6ebf9b0be7dc508a6e81f88
greense <irenelizeth@gmail.com>
2017-06-04 23:40:49 -0400

    updating to find tests names of tests throwing exceptions

--- find_name_testcases.sh
+++ find_name_testcases.sh
@@ -12,9 +12,10 @@ fi
 APP_TEST_DIR=$1
 #APP_TEST_DIR="test/jdepend/framework/"
 
+GREP_COMMAND="grep -r -o -e '\test[[:alnum:]]*[_[[:alnum:]]*]*[\(][\)][:blank: ][\throws]' -r -o -e '\test[[:alnum:]]*[_[[:alnum:]]*]*[\(][\)][ ][\{]'$APP_TEST_DIR | grep -o -e '[\/[:alnum:]_*]*[.]\java[:][[:alnum:]_*]*\test[[:alnum:]]*[_[[:alnum:]]*]*'"
 
-GREP_COMMAND="grep -r -o -e '\test[[:alnum:]]*[_[[:alnum:]]*]*[\(][\)][ ][\{]' $APP_TEST_DIR | grep -o -e '[\/[:alnum:]_*]*[.]\java[:][[:alnum:]_*]*\test[[:alnum:]]*[_[[:alnum:]]*]*'"
 
+#GREP_COMMAND="grep -r -o -e '\test[[:alnum:]]*[_[[:alnum:]]*]*[\(][\)][ ][\{]' $APP_TEST_DIR | grep -o -e '[\/[:alnum:]_*]*[.]\java[:][[:alnum:]_*]*\test[[:alnum:]]*[_[[:alnum:]]*]*'"
 # grep -r -o -e '\test[[:alnum:]]*[\(]' src/test/ | grep -o -e '[\/[:alnum:]_*]*[.]\java[:][[:alnum:]_*]*\test[[:alnum:]_*]*'
 # grep -r -o -e '\test[[:alnum:]]*[\(]' src/test/ | grep -o -e '[\/[:alnum:]_*]*[.][[:alnum:]]*[:][[:alnum:]_*[:alnum:]]*\test[[:alnum:]_*[:alnum:]]*'
 # grep -r -H -o -e '\test[[:alnum:]]*[\(]' $APP_TEST_DIR | grep -o -e '[[:alnum:]]*[.][[:alnum:]]*[:]\test[[:alnum:]]*'
